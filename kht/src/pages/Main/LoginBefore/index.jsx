22c7a7bf57e2b51a3ecc7791df68ed248a315567
downpour07 <choiheeyoung10@gmail.com>
2024-07-10 09:34:15 +0900

    📑 :: Before Login page

---
+++ kht/src/pages/Main/LoginBefore/index.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { styled } from "styled-components";
+
+function LoginBeforePage() {
+
+  return (
+    <>LoginBefore</>
+  );
+}
+
+export default LoginBeforePage;
