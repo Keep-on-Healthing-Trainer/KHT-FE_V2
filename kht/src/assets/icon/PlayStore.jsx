89f66c69f2a94586a40e1b128d8861b93d0c7f4c
downpour07 <choiheeyoung10@gmail.com>
2024-07-10 10:32:41 +0900

    📑 :: icon 추가

---
+++ kht/src/assets/icon/PlayStore.jsx
@@ -0,0 +1,11 @@
+const PlayStore = (props) => {
+    return (
+<svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M21.5 11.634C22.1667 12.0189 22.1667 12.9811 21.5 13.366L8.75 20.7272C8.08333 21.1121 7.25 20.631 7.25 19.8612L7.25 5.13878C7.25 4.36898 8.08333 3.88786 8.75 4.27276L21.5 11.634Z" stroke="white" stroke-width="2"/>
+<line x1="8.01966" y1="20.8108" x2="18.8247" y2="9.58196" stroke="white" stroke-width="2"/>
+<line x1="18.8307" y1="15.0554" x2="8.02564" y2="4.09146" stroke="white" stroke-width="2"/>
+</svg>
+    );
+  };
+  
+  export default PlayStore;
