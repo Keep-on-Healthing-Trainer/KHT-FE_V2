5ce3f191c42e31bcfc1d3e21a0e77b1da396fb04
downpour07 <choiheeyoung10@gmail.com>
2024-07-10 09:34:34 +0900

    📑 :: Search page

---
+++ kht/src/pages/Search/Search/index.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { styled } from "styled-components";
+
+function SearchPage() {
+
+  return (
+    <>Search</>
+  );
+}
+
+export default SearchPage;
