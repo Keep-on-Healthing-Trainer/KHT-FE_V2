74db8b4279dd6ca2dec6d090fc9aadcfb4bb3429
downpour07 <choiheeyoung10@gmail.com>
2024-07-10 09:32:07 +0900

    📑 :: styled 추가

---
+++ kht/src/styles/theme.js
@@ -0,0 +1,9 @@
+export const color = {
+    White: "#FFFFFF",
+    Gray: ["#FAFAFA", "#F4F4F5", "#E4E4E7", "#D4D4D8", "#A1A1AA", "#3F3F46"],
+    Black: "#000000",
+    Blue: ["#E7EEF6", "#CEDCEC", "#B6CBE3", "#9EBADA", "#85A8D1", "#6D97C7", "#5585BE", "#2463AC", "#0C51A2", "#0A386E", "#0075FF"],
+    Gold: "#F0E051",
+    Silver: "#DEDEDE",
+    Copper: "#DDB087"
+};
