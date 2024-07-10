032bf557da27704f9202b951129f5817cbc20c43
downpour07 <choiheeyoung10@gmail.com>
2024-07-10 14:05:58 +0900

    📝 :: Search page 퍼블리싱

--- kht/src/pages/Search/Search/index.jsx
+++ kht/src/pages/Search/Search/index.jsx
@@ -1,11 +1,22 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { styled } from "styled-components";
+
+import * as S from "./styled";
+
+import State from "../../../components/State";
+import SearchInput from "../../../components/Input/Search";
 
 function SearchPage() {
 
   return (
-    <>Search</>
+    <>
+      <S.Main>
+        <S.Center>
+          <State innerText="USER SEARCH" />
+          <SearchInput />
+        </S.Center>
+      </S.Main>
+    </>
   );
 }
 
---
+++ kht/src/pages/Search/Search/styled.jsx
@@ -0,0 +1,20 @@
+import { styled } from "styled-components";
+import { color } from "../../../styles/theme";
+
+export const Main = styled.main`
+  width: 100%;
+  height: 94vh;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Center = styled.article`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  row-gap: 10vh;
+`
