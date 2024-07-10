114cf7cbd01939878d2f28874536404db063ada6
downpour07 <choiheeyoung10@gmail.com>
2024-07-10 09:34:50 +0900

    📑 :: Search Result page

---
+++ kht/src/pages/Search/Result/index.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { styled } from "styled-components";
+
+function ResultPage() {
+
+  return (
+    <>Result</>
+  );
+}
+
+export default ResultPage;
