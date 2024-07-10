9929e626f7280167e92cb886c091471e887b486d
downpour07 <choiheeyoung10@gmail.com>
2024-07-10 14:04:38 +0900

    📑 :: SearchIcon icon 추가

---
+++ kht/src/assets/icon/SearchIcon.jsx
@@ -0,0 +1,11 @@
+const SearchIcon = (props) => {
+    return (
+<svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="8.5" cy="8.5" r="7.5" stroke="#0075FF" stroke-width="2"/>
+<rect x="13" y="14.4141" width="2" height="10.6969" rx="1" transform="rotate(-45 13 14.4141)" fill="#0075FF"/>
+</svg>
+
+    );
+  };
+  
+  export default SearchIcon;
