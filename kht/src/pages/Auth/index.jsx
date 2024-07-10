00126cb7bc7cc624f84c26c60a214a57cfaddf23
downpour07 <choiheeyoung10@gmail.com>
2024-07-10 09:43:59 +0900

    📝 :: Login page 코드 분할

--- kht/src/pages/Auth/index.jsx
+++ kht/src/pages/Auth/index.jsx
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { styled } from "styled-components";
 
-import { color } from "../../styles/theme";
+import * as S from "./styled";
 
 import Input from "../../components/Input/Input";
 import State from "../../components/State";
@@ -12,43 +11,18 @@ function LoginPage() {
 
   return (
     <>
-      <Main>
-        <Center>
+      <S.Main>
+        <S.Center>
           <State innerText="LOGIN" />
-          <InputDiv>
+          <S.InputDiv>
             <Input innerText="아이디" state="text" />
             <Input innerText="비밀번호" state="password" />
-          </InputDiv>
+          </S.InputDiv>
           <Button innerText="로그인" />
-        </Center>
-      </Main>
+        </S.Center>
+      </S.Main>
     </>
   );
 }
-
-export const Main = styled.main`
-  width: 100%;
-  height: 94vh;
-
-  display: flex;
-  justify-content: center;
-`;
-
-export const InputDiv = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-
-  row-gap: 3vh;
-`;
-
-export const Center = styled.article`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  row-gap: 12vh;
-`
 
 export default LoginPage;
