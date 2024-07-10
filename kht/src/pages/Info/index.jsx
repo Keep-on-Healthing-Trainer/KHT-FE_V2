e465d4c44bcaaf499e84e8a244bff1da44ab1496
downpour07 <choiheeyoung10@gmail.com>
2024-07-10 14:53:55 +0900

    📝 :: Info page 퍼블리싱

--- kht/src/pages/Info/index.jsx
+++ kht/src/pages/Info/index.jsx
@@ -1,11 +1,42 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { styled } from "styled-components";
+
+import * as S from "./styled";
 
 function InfoPage() {
 
   return (
-    <></>
+    <>
+      <S.Main>
+        <S.Profile>
+          <S.ProfileImg></S.ProfileImg>
+          <S.Id>TESTTEST</S.Id>
+          <S.Text>이기혁</S.Text>
+        </S.Profile>
+        <S.RightDiv>
+          <S.Ranking>
+            <S.Text>내 랭킹</S.Text>
+            <S.SelectDiv>
+              <S.Select>
+                <S.Label>윗몸일으키기</S.Label>
+                <S.Count>120위</S.Count>
+              </S.Select>
+              <S.Select>
+                <S.Label>스쿼트</S.Label>
+                <S.Count>1234위</S.Count>
+              </S.Select>
+              <S.Select>
+                <S.Label>팔굽혀펴기</S.Label>
+                <S.Count>654위</S.Count>
+              </S.Select>
+            </S.SelectDiv>
+          </S.Ranking>
+          <S.Grape>
+            <S.Text>내 그래프</S.Text>
+          </S.Grape>
+        </S.RightDiv>
+      </S.Main>
+    </>
   );
 }
 
--- kht/src/pages/Info/styled.jsx
+++ kht/src/pages/Info/styled.jsx
@@ -1,1 +1,84 @@
 import { styled } from "styled-components";
+import { color } from "../../styles/theme";
+
+export const Main = styled.main`
+  width: 100%;
+  height: 94vh;
+  gap: 2vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Profile = styled.article`
+  width: 15vw;
+  height: 70vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  row-gap: 1vh;
+`
+
+export const ProfileImg = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: gray;
+  border-radius: 50%;
+`
+
+export const Id = styled.div`
+  font-size: 16px;
+  font-weight: 400;
+  color: ${color.Black};
+`
+
+export const Text = styled.div`
+  font-size: 22px;
+  font-weight: 600;
+  color: ${color.Black};
+`
+
+export const RightDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 5vh;
+`
+
+export const Ranking = styled.article`
+  width: 50vw;
+  height: 25vh;
+  row-gap: 10vh;
+`
+
+export const Label = styled.div`
+  font-size: 16px;
+  font-weight: 400;
+  color: ${color.Black};
+`
+
+export const Count = styled.div`
+  font-size: 18px;
+  font-weight: 600;
+  color: ${color.Black};
+`
+
+export const Select = styled.div`
+  width: 15vw;
+  height: 10vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  row-gap: 1vh;
+`
+
+export const SelectDiv = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+
+export const Grape = styled.article`
+  width: 50vw;
+  height: 40vh;
+`
