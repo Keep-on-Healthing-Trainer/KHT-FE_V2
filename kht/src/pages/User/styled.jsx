14c06eb617b032d996501d387c6b36f879ea6c7e
downpour07 <choiheeyoung10@gmail.com>
2024-07-11 10:23:51 +0900

    📝 :: Signup page 반응형

--- kht/src/pages/User/styled.jsx
+++ kht/src/pages/User/styled.jsx
@@ -1,12 +1,16 @@
 import { styled } from "styled-components";
 import { color } from "../../styles/theme";
+import { breakpoints } from "../../styles/device";
 
 export const Main = styled.main`
   width: 100vw;
   height: 94vh;
-
   display: flex;
   justify-content: center;
+
+  @media only screen and (max-width: ${breakpoints.small}) {
+    width: 700px;
+  }
 `;
 
 export const InputDiv = styled.div`
@@ -14,7 +18,6 @@ export const InputDiv = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-
   row-gap: 3vh;
 `;
 
