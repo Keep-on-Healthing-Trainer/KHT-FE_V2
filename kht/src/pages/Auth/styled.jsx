8020edf41e5f81b5cb0ce794ed040e4ab538ef4c
downpour07 <choiheeyoung10@gmail.com>
2024-07-11 09:55:24 +0900

    📝 :: Login page 반응형

--- kht/src/pages/Auth/styled.jsx
+++ kht/src/pages/Auth/styled.jsx
@@ -1,12 +1,16 @@
 import { styled } from "styled-components";
+import { breakpoints } from "../../styles/device";
 import { color } from "../../styles/theme";
 
 export const Main = styled.main`
   width: 100%;
   height: 94vh;
-
   display: flex;
   justify-content: center;
+
+  @media only screen and (max-width: ${breakpoints.small}) {
+    width: 700px;
+  }
 `;
 
 export const InputDiv = styled.div`
@@ -14,7 +18,6 @@ export const InputDiv = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-
   row-gap: 3vh;
 `;
 
