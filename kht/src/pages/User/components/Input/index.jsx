2ed3c659c987eaf3a4e38b0c7d7ae63949b67465
downpour07 <choiheeyoung10@gmail.com>
2024-07-22 16:02:06 +0900

    ⚡️ :: Signup page Input value

--- kht/src/pages/User/components/Input/index.jsx
+++ kht/src/pages/User/components/Input/index.jsx
@@ -4,14 +4,48 @@ import { styled } from "styled-components";
 import Input from "../../../../components/Input/Input";
 
 const InputComponents = ( props ) => {
+  const [signupData, setSignupData] = useState({
+    name: "",
+    number: "",
+    id: "",
+    password: "",
+    passwordCheck: "",
+  });
+
+  const handleInputChange = (text, field) => {
+    setSignupData(prevData => ({
+      ...prevData,
+      [field]: text
+    }));
+  }
 
   return (
     <InputDiv>
-        <Input innerText="이름" state="text" />
-        <Input innerText="전화번호" state="text" />
-        <Input innerText="아이디" state="text" />
-        <Input innerText="비밀번호" state="password" />
-        <Input innerText="비밀번호 확인" state="password" />
+        <Input
+        innerText="이름"
+        state="text" 
+        onGetInText={(text) => handleInputChange(text, "name")}
+        />
+        <Input
+        innerText="전화번호"
+        state="text" 
+        onGetInText={(text) => handleInputChange(text, "number")}
+        />
+        <Input
+        innerText="아이디"
+        state="text" 
+        onGetInText={(text) => handleInputChange(text, "id")}
+        />
+        <Input
+        innerText="비밀번호"
+        state="password" 
+        onGetInText={(text) => handleInputChange(text, "password")}
+        />
+        <Input
+        innerText="비밀번호 확인"
+        state="password" 
+        onGetInText={(text) => handleInputChange(text, "passwordCheck")}
+        />
     </InputDiv>
   );
 }
